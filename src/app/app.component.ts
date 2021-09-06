c35c18ebc54d8bfa86f451b5d23406dac49125a2
DanielEcheverry96 <dfecheverryg@unal.edu.co>
2021-09-05 19:03:19 -0500

    Add components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,20 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  contador: number;
+
+  constructor() {
+    this.contador = 10;
+  }
+
+  incrementar() {
+    this.contador++;
+  }
+
+  decrementar() {
+    this.contador--;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { HijoComponent } from './contador/hijo/hijo.component';
+import { NietoComponent } from './contador/nieto/nieto.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HelloComponent, HijoComponent, NietoComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
